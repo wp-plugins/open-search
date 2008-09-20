9cfa5831cddbe763da928fbf68da26831488b26c
Darathor <Darathor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-20 15:21:56 +0000

    [DOC] Complete information in the plugin header.
    git-svn-id: https://plugins.svn.wordpress.org/open-search/trunk@65659 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- open-search.php
+++ open-search.php
@@ -1,9 +1,10 @@
 <?php
 /*
 Plugin Name: Open search
-Plugin URI: http://wp.darathor.com
-Author: Darathor < darathor@free.fr >
-Description: Generates automatically the Open search document for your WordPress blog.
+Plugin URI: http://wp.darathor.com/mes-plugins-maison/open-search/
+Author: Darathor
+Author URI: http://wp.darathor.com
+Description: Generates automatically the Open search document for your WordPress blog. It allows visitors to add your blog in search engine list of their browser.
 Version: 1.0.1
 */
 
